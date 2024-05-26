b24198471f9f8e8c6786335fc272f2a980ac4258
johnh <jhuan47@wgu.edu>
2024-05-26 12:18:22 -0400

    adds ability to replay the game from the window instead of having to refresh through terminal

--- test.py
+++ test.py
@@ -14,29 +14,29 @@ word_list_form = []
 
 lives = 5
 
-def choose_difficulty():
+def choose_difficulty(start_button):
 
     # get rid of the start game button
     start_button.destroy()
 
     # word length instruction label
-    word_display = Label(root, text="Choose the length of word you want", font=("Arial", 20), pady=20)
-    word_display.pack()
+    word_display = Label(root, text="Choose the length of word you want", font=("Arial", 20))
+    word_display.pack(ipady=100)
 
     # generate 3 buttons to choose word length
-    short_but = Button(root, text="Short word", command=lambda:generate_word(4, short_but, med_but, long_but, word_display))
-    short_but.pack(pady=20)
-    med_but = Button(root, text="Medium word", command=lambda:generate_word(7, short_but, med_but, long_but, word_display))
-    med_but.pack(pady=20)
-    long_but = Button(root, text="Long word", command=lambda:generate_word(15, short_but, med_but, long_but, word_display))
-    long_but.pack(pady=20)
+    short_word_button = Button(root, text="Short word", command=lambda:generate_word(4, short_word_button, medium_word_button, long_word_button, word_display))
+    short_word_button.pack(pady=20)
+    medium_word_button = Button(root, text="Medium word", command=lambda:generate_word(7, short_word_button, medium_word_button, long_word_button, word_display))
+    medium_word_button.pack(pady=20)
+    long_word_button = Button(root, text="Long word", command=lambda:generate_word(15, short_word_button, medium_word_button, long_word_button, word_display))
+    long_word_button.pack(pady=20)
 
-def generate_word(word_length, short_but, med_but, long_but, word_display):
+def generate_word(word_length, short_word_button, medium_word_button, long_word_button, word_display):
     
     # destroy buttons and instruction label
-    short_but.destroy()
-    med_but.destroy()
-    long_but.destroy()
+    short_word_button.destroy()
+    medium_word_button.destroy()
+    long_word_button.destroy()
     word_display.destroy()
 
     # Filter out words to get a word with appropriate length
@@ -46,7 +46,7 @@ def generate_word(word_length, short_but, med_but, long_but, word_display):
         while len(test_word) > 4:
             test_word = str(random.choice(words)).replace("b", "", 1).replace('\'', "")
     elif word_length == 7:
-        while len(test_word) <= 4 and len(test_word) < 7:
+        while len(test_word) <= 4 and len(test_word) > 7:
             test_word = str(random.choice(words)).replace("b", "", 1).replace('\'', "")
     elif word_length == 15: 
         while len(test_word) <=7:
@@ -57,6 +57,9 @@ def generate_word(word_length, short_but, med_but, long_but, word_display):
 def start_game(hangman_word):
 
     global word_list_form
+
+    result_label = Label(root, text="", font=('Arial', 20))
+    result_label.pack(pady=10)
 
     canvas = Canvas(root, width=1400, height=800) 
 
@@ -73,10 +76,10 @@ def start_game(hangman_word):
     word_display = Label(root, text="_ " * len(hangman_word), font=("Arial", 20), pady=20)
     word_display.pack()
 
-    hangman_game(canvas, hangman_image, word_display, hangman_word)
+    hangman_game(canvas, hangman_image, word_display, hangman_word, result_label)
 
 
-def hangman_game(canvas, hangman_image, word_display, hangman_word):
+def hangman_game(canvas, hangman_image, word_display, hangman_word, result_label):
 
     guessed_list = Label(text=f'{guessed_letters}', font=('Arial', 20), pady=20)
     guessed_list.pack()
@@ -84,11 +87,11 @@ def hangman_game(canvas, hangman_image, word_display, hangman_word):
     # listens to enter key and passes user input to submit input function
     entry = Entry(root, width=50)
     entry.pack()
-    entry.bind('<Return>', lambda event: submit_input(entry, canvas, word_display, hangman_word, hangman_image, guessed_list))
+    entry.bind('<Return>', lambda event: submit_input(entry, canvas, word_display, hangman_word, hangman_image, guessed_list, result_label))
 
 
-def submit_input(entry, canvas, word_display, hangman_word, hangman_image, guessed_list):
-    global lives, result_label
+def submit_input(entry, canvas, word_display, hangman_word, hangman_image, guessed_list,result_label):
+    global lives
     result_label.config(text="")
     user_input = entry.get().strip().lower()
     
@@ -102,6 +105,9 @@ def submit_input(entry, canvas, word_display, hangman_word, hangman_image, guess
         result_label.config(text=f"YOU WIN! THE WORD WAS: {hangman_word.upper()}", font=("Arial", 15))
         word_display.config(text=f"{hangman_word}")
         entry.destroy()
+        replay_button = Button(root, text="Replay", command=reset_game)
+        replay_button.pack()
+        return None
 
     elif user_input != hangman_word and len(user_input) > 1 and lives >=0:
         result_label.config(text=f"Wrong! The word is not {user_input}, guess again", font=("Arial", 15))
@@ -119,8 +125,9 @@ def submit_input(entry, canvas, word_display, hangman_word, hangman_image, guess
                 canvas.itemconfig(hangman_image, image=hangman_stages[7])
                 result_label.config(text=f"YOU WIN! THE WORD WAS: {hangman_word.upper()}", font=("Arial", 15))
                 entry.destroy()
-                # TODO
-                    # CREATE A BUTTON TO REPLAY
+                replay_button = Button(root, text="Replay", command=reset_game)
+                replay_button.pack()
+                return None
         else:
             guessed_letters.append(user_input)
             guessed_list.config(text=f'Guessed Letters/Words: {", ".join(guessed_letters)}')
@@ -131,8 +138,9 @@ def submit_input(entry, canvas, word_display, hangman_word, hangman_image, guess
                 canvas.itemconfig(hangman_image, image=hangman_stages[6])
                 result_label.config(text=f"OUT OF LIVES, THE WORD WAS {hangman_word.upper()}", font=("Arial", 15))
                 entry.destroy()
-                # TODO
-                    # CREATE A BUTTON TO REPLAY
+                replay_button = Button(root, text="Replay", command=reset_game)
+                replay_button.pack()
+                return None
     
     entry.delete(0, 'end')
 
@@ -145,13 +153,25 @@ def update_blanks(word_display, hangman_word, user_input):
                 word_list_form[index] = x
     word_display.config(text=f"{' '.join(word_list_form)}")
 
+def reset_game():
+    global guessed_letters, lives, word_list_form
+
+    for widget in root.winfo_children():
+        widget.destroy()
+    guessed_letters = []
+    lives = 5
+    word_list_form = []
+    start_button = Button(root, text="Start Game", command=lambda: choose_difficulty(start_button))
+    start_button.pack(pady=500)
+    
+
 
 root = Tk()
 root.title('Hangman')
 root.minsize(width=2000, height=1500)
 
-start_button = Button(root, text="Start Game", command=choose_difficulty)
-start_button.pack()
+start_button = Button(root, text="Start Game", command=lambda:choose_difficulty(start_button))
+start_button.pack(pady=500)
 
 
 hangman_stages = [
@@ -164,8 +184,5 @@ hangman_stages = [
     PhotoImage(file="images/hangman_lose.png"),
     PhotoImage(file="images/hangman_win.png")
 ]
-
-result_label = Label(root, text="", font=('Arial', 20))
-result_label.pack(pady=10)
 
 root.mainloop()
